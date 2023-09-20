3db4cc72dafe2d4f9091c3bcbf550605d3693dbc
Dugege007 <351080175@qq.com>
2023-09-21 00:03:23 +0800

    QFramework的第一个脚本

---
+++ Assets/Scripts/Game/Player.Designer.cs
@@ -0,0 +1,10 @@
+// Generate Id:25fefa96-4643-406a-86c5-e214c1756575
+using UnityEngine;
+
+namespace ProjectSurvivor
+{
+	public partial class Player
+	{
+
+	}
+}
---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using QFramework;
+
+namespace ProjectSurvivor
+{
+	public partial class Player : ViewController
+	{
+		private void Start()
+		{
+			"Hello QFramework!".LogInfo();
+		}
+	}
+}
