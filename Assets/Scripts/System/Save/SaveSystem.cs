b8e5d3beaa1d64078834d51b23c84f42d59092da
Dugege007 <351080175@qq.com>
2023-09-27 20:16:06 +0800

    047. 金币升级界面支持存档
    加入简单的存档数据展示功能

--- Assets/Scripts/System/Save/SaveSystem.cs
+++ Assets/Scripts/System/Save/SaveSystem.cs
@@ -1,13 +1,28 @@
 ﻿using QFramework;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ProjectSurvivor
 {
     public class SaveSystem : AbstractSystem
     {
+        public HashSet<string> Keys = new HashSet<string>();
+
         protected override void OnInit()
         {
-
+            ActionKit.OnGUI.Register(() =>
+            {
+                if (Input.GetKey(KeyCode.L))
+                {
+                    // 展示数据
+                    foreach (var key in Keys)
+                    {
+                        GUILayout.Label(key + ": " + PlayerPrefs.GetInt(key));
+                        GUILayout.Label(key + ": " + PlayerPrefs.GetString(key));
+                        GUILayout.Label(key + ": " + PlayerPrefs.GetFloat(key));
+                    }
+                }
+            });
         }
 
         public void Save()
@@ -22,31 +37,49 @@ namespace ProjectSurvivor
 
         public void SaveBool(string key, bool value)
         {
+            Keys.Add(key);
             PlayerPrefs.SetInt(key, value ? 1 : 0);
         }
 
         public bool LoadBool(string key, bool defaultValue = false)
         {
+            Keys.Add(key);
             return PlayerPrefs.GetInt(key, defaultValue ? 1 : 0) == 1;
         }
 
         public void SaveInt(string key, int value)
         {
+            Keys.Add(key);
             PlayerPrefs.SetInt(key, value);
         }
 
         public int LoadInt(string key, int defaultValue = 0)
         {
+            Keys.Add(key);
             return PlayerPrefs.GetInt(key, defaultValue);
+        }
+
+        public void SaveFloat(string key, float value)
+        {
+            Keys.Add(key);
+            PlayerPrefs.SetFloat(key, value);
+        }
+
+        public float LoadFloat(string key, float defaultValue = 0f)
+        {
+            Keys.Add(key);
+            return PlayerPrefs.GetFloat(key, defaultValue);
         }
 
         public void SaveString(string key, string value)
         {
+            Keys.Add(key);
             PlayerPrefs.SetString(key, value);
         }
 
         public string LoadString(string key, string defaultValue = default)
         {
+            Keys.Add(key);
             return PlayerPrefs.GetString(key, defaultValue);
         }
     }
